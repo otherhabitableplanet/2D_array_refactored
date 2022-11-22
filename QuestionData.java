1713780e3c956840ce364fa642a166a1a14d7b93
otherhabitableplanet <cameronchale@gmail.com>
2022-11-22 14:26:22 -0500

    Actual main more or less ready and qData fixed

--- QuestionData.java
+++ QuestionData.java
@@ -1,12 +1,13 @@
 class QuestionData {
-  
+  protected String fileName;
+  protected String[] questionData;
   public static void questionArray(){
     String [][] questionData = new String[countLines(this.fileName)][];
   }
 
   public QuestionData(String fileName){
-  this.fileName = fileName;
-  QuestionDataReader reader = new QuestionDataReader(fileName);
-  this.QuestionData = reader.readQuestionsOverseer();
-} 
+    this.fileName = fileName;
+    QuestionDataReader reader = new QuestionDataReader(fileName);
+    this.questionData = reader.readQuestionsOverseer();
+  }
 }
