892cb142bdd1fa8995ed4f8a42fae78c0da2f7b0
AshleighW <60052951+Ash-uwu@users.noreply.github.com>
2022-11-21 21:35:59 -0500

    Update AnswerData.java

--- AnswerData.java
+++ AnswerData.java
@@ -1,6 +1,9 @@
 public class AnswerData{
+    //constructor for answerData file name
+    public answerDataReader(){
+        this.fileName = getName();
+        this.AnswerData = new String[countLines(this.fileName)];
+    }
     //stores the cleaned up answer file from AnswerDataReader, and stores it into 2D array
     String[][] answers = answers();
-    //String that will hold the name of the answer file from the main
-    //String answerDataFileName = ;
 }
