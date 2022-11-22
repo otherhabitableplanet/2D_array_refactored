36945a078b0c63321e158902a6c3fcfca33c0f75
AshleighW <60052951+Ash-uwu@users.noreply.github.com>
2022-11-21 22:01:17 -0500

    Added constructor for responseData

--- ResponseData.java
+++ ResponseData.java
@@ -1,5 +1,17 @@
 public class ResponseData{
+    protected String fileName;
+    //going to be a string list called ansData
+    protected String[][] ResponseData;
+    //constructor for answerData file name
+    public ResponseData(String fileName){
+        //takes file name, then gives the reader
+        this.fileName = fileName;
+        ResponseDataReader reader = new ResponseDataReader(fileName);
+        //will take answer data file from the answer reader
+        //runs this as the 2D array
+        this.ResponseData = reader.readAnswersOverseer();
+    }
     //grabs readResponses method, and put its data into responseData 2D array
-    String [][] responseData = readResponses();
+    //String [][] responseData = readResponses();
     compareAnswers();
 }
