0ae602926b7aa547f12c3b817d64b7330e350dcc
Zara <116698765+ZaraKhann@users.noreply.github.com>
2022-11-22 15:14:16 -0500

    added comments

--- QuestionData.java
+++ QuestionData.java
@@ -1,13 +1,15 @@
 class QuestionData {
   protected String fileName;
+  //initialize array 
   protected String[] questionData;
-  public static void questionArray(){
-    String [][] questionData = new String[countLines(this.fileName)][];
-  }
-
+ 
+  // constructor for questionData
   public QuestionData(String fileName){
+    //takes file name and gives it to reader
     this.fileName = fileName;
+    // takes question data from question data reader
     QuestionDataReader reader = new QuestionDataReader(fileName);
+    // runs overseer method that stores questionData in an array
     this.questionData = reader.readQuestionsOverseer();
   }
 }
