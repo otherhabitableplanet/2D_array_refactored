b0b9bce5f97cd6b059de63ba2155cc5809f88e31
Zara <116698765+ZaraKhann@users.noreply.github.com>
2022-11-22 15:11:18 -0500

    added comments

--- StudentData.java
+++ StudentData.java
@@ -1,12 +1,16 @@
 
 class StudentData {
   protected String fileName;
+  // initialize 2D array for StudentData
   protected String[][] StudentData;
   
   // constructor for studentData 
   public StudentData(String fileName){
+    // takes file name and gives it to reader
     this.fileName = fileName;
+    // takes student data from student data reader
     StudentDataReader reader = new StudentDataReader(fileName);
+    // runs overseer method that stores studentData in a 2D array
     this.StudentData = reader.readStudentsOverseer();
     }
 }
